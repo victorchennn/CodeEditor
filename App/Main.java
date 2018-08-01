b68069d11d64855d276b0d864bddf7999368945b
Chen <victorchennn@gmail.com>
2018-08-01 14:13:04 +0800

    update

--- App/Main.java
+++ App/Main.java
@@ -3,6 +3,6 @@ package App;
 public class Main {
     public static void main(String...args) {
         SignIn signIn = new SignIn();
-        signIn.signin();
+        signIn.init();
     }
 }
--- App/SignIn.java
+++ App/SignIn.java
@@ -1,17 +1,35 @@
 package App;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JComponent;
+import javax.swing.WindowConstants;
+import javax.swing.JFileChooser;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
 
-class SignIn{
+class SignIn {
 
-    void signin() {
-        init();
-    }
-
-    private void init() {
+    /** Initiate the whole Sign-In interface. */
+    void init() {
         frame = new JFrame("Victor");
         frame.setSize(700, 450);
         frame.setResizable(false);
@@ -30,6 +48,7 @@ class SignIn{
         frame.setVisible(true);
     }
 
+    /** Add labels, using MouseListener to open the file user saved before. */
     private void addLabel() {
         JLabel l1 = new JLabel("EMAIL");
         l1.setBounds(315, 30, 300, 40);
@@ -47,16 +66,39 @@ class SignIn{
         l3.setBounds(340, 340, 200, 30);
 
         JLabel l4 = new JLabel("Sign in");
-        l4.setFont(new Font("Courier", Font.PLAIN, 14));
+        l4.setFont(new Font("Courier", Font.PLAIN, 13));
         l4.setForeground(Color.WHITE);
         l4.setBounds(540, 340, 100, 30);
-
+        l4.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        l4.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                JFileChooser fc = new JFileChooser();
+                String current_path = System.getProperty("user.dir");
+                fc.setCurrentDirectory(new File(current_path));
+                int value = fc.showOpenDialog(null);
+                if (value == JFileChooser.APPROVE_OPTION) {
+                    File file = fc.getSelectedFile();
+                    FileReader fr = null;
+                    try {
+                        fr = new FileReader(file);
+                    } catch (FileNotFoundException exp) {
+                        /* ... */
+                    }
+                    BufferedReader br = new BufferedReader(fr);
+                    /*
+                     * ....
+                     */
+                }
+            }
+        });
         frame.add(l1);
         frame.add(l2);
         frame.add(l3);
         frame.add(l4);
     }
 
+    /** Add textfield EMAIL and passwordfield PASSWORD. */
     private void addText() {
         email = new JTextField();
         email.setBounds(314, 70, 310, 50);
@@ -76,6 +118,8 @@ class SignIn{
         frame.add(password);
     }
 
+    /** Add GET STARTED button to current frame, using ActionListener to
+     * check if email and password can match. */
     private void addButton() {
         JButton getstarted = new JButton(new ImageIcon("button.jpg"));
         getstarted.setBounds(314, 280, 310, 60);
@@ -88,17 +132,18 @@ class SignIn{
                 for (char c : password.getPassword()) {
                     s2 += c;
                 }
-
+                /*
+                 * ....
+                 */
             }
         });
         frame.add(getstarted);
     }
 
     public class Paint extends JComponent{
-
         @Override
         public void paintComponent(Graphics g) {
-            Toolkit t=Toolkit.getDefaultToolkit();
+            Toolkit t = Toolkit.getDefaultToolkit();
             Image i = t.getImage("background.jpg");
             g.drawImage(i, 0, 0, this);
 
@@ -109,9 +154,12 @@ class SignIn{
 
     }
 
+    /** User's email address. */
     private JTextField email;
 
+    /** User's password. */
     private JPasswordField password;
 
+    /** Current Sign-In interface. */
     private JFrame frame;
 }
